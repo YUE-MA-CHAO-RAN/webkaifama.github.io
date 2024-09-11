b6cfd12d9d1e542e7446c9e331a258872f6baff5
YUE-MA-CHAO-RAN <3034978390@qq.com>
2024-09-11 19:28:22 +0800

    Add files via upload

---
+++ js/haha.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var serviceButton = document.getElementById('serviceButton');
+    
+    serviceButton.addEventListener('click', function() {
+        alert('服务正在开发中，敬请期待！');
+    });
+});
